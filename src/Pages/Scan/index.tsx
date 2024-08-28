f75f5348bc60cfeab9bca67d7dcbfd269d1c8b1d
oluseunakintayo <oluseun.akintayo@gmail.com>
2024-08-28 15:07:13 +0100

    Feat: Added QR Code scanning page

--- src/Pages/Scan/index.tsx
+++ src/Pages/Scan/index.tsx
@@ -1,22 +1,47 @@
+import axios, { AxiosRequestConfig } from 'axios';
 import React from 'react';
 import { useLocation, useSearchParams } from 'react-router-dom';
 
+interface IScanResult {
+  success: boolean;
+  url: string;
+}
 
 const Scan = () => {
   const location = useLocation().search;
-  console.log(location)
   const [params,] = useSearchParams();
   const id = params.get("url-id");
 
+  const scan = async (urlId: string) => {
+    const options: AxiosRequestConfig = {
+      method: "GET",
+      url: `${import.meta.env.VITE_BACKEND_URL}/qrcode/scan?Id=${urlId}`,
+    }
+    try {
+      const res = await axios.request(options);
+      const data: IScanResult = res.data;
+      if(data.success) window.location.replace(data.url);
+      return;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   React.useEffect(() => {
-    console.log(id);
+    if(id) scan(id);
   }, [id]);
 
   return (
     <section className='h-screen grid place-items-center'>
       <div className='p-4'>
-        <h3><span className='text-2xl animate-pulse'>Scanning QR Code</span></h3>
-        <p className='text-center animate-pulse'>Please wait...</p>
+        {
+          (location.trim() === '' || id?.trim() === '') ?
+          <h3 className='text-2xl'>URL ID cannot be empty</h3> :
+          <>
+            <h3><span className='text-2xl animate-pulse'>Scanning QR Code</span></h3>
+            <p className='text-center animate-pulse'>Please wait...</p>
+          </>
+        }
       </div>
     </section>
   )
